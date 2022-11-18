a43998b2d3ffabd48971953e69fd277a729fb348
Elias Ondrej <info@eliasondrej.com>
2022-11-18 13:29:23 +0100

    prettier formatting

--- application/.stylelintrc.cjs
+++ application/.stylelintrc.cjs
@@ -1,20 +1,17 @@
 module.exports = {
-    "extends": "@media-factory-cz/stylelint-config",
-    "rules": {
-        "selector-pseudo-class-no-unknown": [
+    extends: '@media-factory-cz/stylelint-config',
+    rules: {
+        'selector-pseudo-class-no-unknown': [
             true,
             {
-                "ignorePseudoClasses": [
-                    "global",
-                    "export"
-                ]
-            }
+                ignorePseudoClasses: ['global', 'export'],
+            },
         ],
-        "selector-class-pattern": null,
-        "max-nesting-depth": 3,
-        "no-descending-specificity": null,
-        "scss/selector-no-redundant-nesting-selector": null,
-        "declaration-block-no-redundant-longhand-properties": null,
-        "shorthand-property-no-redundant-values": null
-    }
-}
+        'selector-class-pattern': null,
+        'max-nesting-depth': 3,
+        'no-descending-specificity': null,
+        'scss/selector-no-redundant-nesting-selector': null,
+        'declaration-block-no-redundant-longhand-properties': null,
+        'shorthand-property-no-redundant-values': null,
+    },
+};
